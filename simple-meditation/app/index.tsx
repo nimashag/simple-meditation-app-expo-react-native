7b68feb4a0dd6b6592ac1092e72e09d1bfafed95
Nimasha Gamage <nimasha.piyumini@gmail.com>
2024-09-12 11:59:40 +0530

    Create splash screen and userRouteHook

--- simple-meditation/app/index.tsx
+++ simple-meditation/app/index.tsx
@@ -1,11 +1,16 @@
-import { View, Text, ImageBackground } from "react-native";
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
 import React from "react";
 import { LinearGradient } from 'expo-linear-gradient';
-
+import { StatusBar } from "expo-status-bar";
 
 import beachImage from "@/assets/meditation-images/beach.webp";
+import CustomButton from "@/components/CustomButton";
+import { useRouter } from "expo-router";
 
 const App = () => {
+
+  const router = useRouter();
+
   return (
     <View className="flex-1">
       <ImageBackground 
@@ -13,7 +18,18 @@ const App = () => {
         resizeMode="cover"
         className="flex-1">
           <LinearGradient className="flex-1" colors={["rgba( 0, 0, 0, 0.4)", "rgba( 0, 0, 0, 0.8)"]}>
-            <Text>App</Text>
+            <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+              <View>
+                <Text className="text-center text-white font-bold text-3xl mt-6">Simple Meditation</Text>
+                <Text className="text-center text-white font-regular text-xs mt-2">Simplifying Meditation for Everyone</Text>
+              </View>
+
+              <View>
+                <CustomButton onPress={() => router.push("/test")} title='Get Started' />
+              </View>
+
+              <StatusBar style="light" />
+            </SafeAreaView>
           </LinearGradient> 
       </ImageBackground>
     </View>
