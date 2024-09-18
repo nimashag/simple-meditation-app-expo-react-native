4f4b264755d884c3aabafd62f54f097de5798394
Nimasha Gamage <nimasha.piyumini@gmail.com>
2024-09-18 10:59:45 +0530

    Create Meditation detail screen

--- simple-meditation/app/_layout.tsx
+++ simple-meditation/app/_layout.tsx
@@ -1,10 +1,28 @@
-import { Slot, Stack } from "expo-router";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+//this will prevent the splash screen from auto hiding until loading all the fonts assets.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    return (
-        <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-        </Stack>
-    );
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  });
+
+  if (!fontsLoaded) return null;
+  if (!fontsLoaded && !error) return null;
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="meditate/[id]" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
---
+++ simple-meditation/app/meditate/[id].tsx
@@ -0,0 +1,74 @@
+import { View, Text, ImageBackground, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import MEDITATION_IMAGES from "@/constants/meditation-images";
+import AppGradient from "@/components/AppGradient";
+import { router, useLocalSearchParams } from "expo-router";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import CustomButton from "@/components/CustomButton";
+
+const Meditate = () => {
+  const { id } = useLocalSearchParams();
+
+  const [secondsRemaining, setSecondRemaining] = useState(10);
+  const [isMeditating, setMeditating] = useState(false);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    //Exit
+    if (secondsRemaining === 0) {
+      setMeditating(false);
+      return;
+    }
+
+    if (isMeditating) {
+      timerId = setTimeout(() => {
+        setSecondRemaining(secondsRemaining - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [secondsRemaining, isMeditating]);
+
+  //Format the time left to ensure two digits are displayed
+  const formattedTimeMinutes = String(
+    Math.floor(secondsRemaining / 60)
+  ).padStart(2, "0");
+  const formattedTimeSeconds = String(secondsRemaining % 60).padStart(2, "0");
+
+  return (
+    <View className="flex-1">
+      <ImageBackground
+        source={MEDITATION_IMAGES[Number(id) - 1]}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["transparent", "rgba(0, 0, 0, 0.8)"]}>
+          <Pressable
+            onPress={() => router.back()}
+            className="absolute top-16 left-6 z-10"
+          >
+            <AntDesign name="leftcircleo" size={50} color="white" />
+          </Pressable>
+          <View className="flex-1 justify-center">
+            <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center">
+              <Text className="text-4xl text-blue-800 font-rmono">
+                {formattedTimeMinutes}:{formattedTimeSeconds}
+              </Text>
+            </View>
+          </View>
+          <View className="mb-5">
+            <CustomButton
+              title="Start Meditation"
+              onPress={() => setMeditating(true)}
+            />
+          </View>
+        </AppGradient>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default Meditate;
